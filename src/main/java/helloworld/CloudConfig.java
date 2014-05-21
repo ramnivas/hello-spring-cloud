7240d918723217a4fe7da072effbd1dc822cdfcc
Ramnivas Laddad <rladdad@gopivotal.com>
2014-05-21 15:57:51 -0700

    Non-ServiceScan

--- src/main/java/helloworld/CloudConfig.java
+++ src/main/java/helloworld/CloudConfig.java
@@ -1,31 +1,36 @@
 package helloworld;
 
+import javax.sql.DataSource;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
-import org.springframework.cloud.config.java.ServiceScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @Configuration
-@ServiceScan
+//@ServiceScan
 @Profile("cloud")
 public class CloudConfig extends AbstractCloudConfig {
-//    @Bean
-//    public ConnectionFactory rabbitConnectionFactory() {
-//        return connectionFactory().rabbitConnectionFactory();
-//    }
-//    
-//    @Bean
-//    public DataSource dataSource() {
-//        return connectionFactory().dataSource();
-//    }
-//
-//    @Bean
-//    public MongoDbFactory mongoDb() {
-//        return connectionFactory().mongoDbFactory();
-//    }
+    @Bean
+    public ConnectionFactory rabbitConnectionFactory() {
+        return connectionFactory().rabbitConnectionFactory();
+    }
+    
+    @Bean
+    public DataSource dataSource() {
+        return connectionFactory().dataSource();
+    }
 
-//    @Bean
-//    public RedisConnectionFactory redisConnectionFactory() {
-//        return connectionFactory().redisConnectionFactory();
-//    }
+    @Bean
+    public MongoDbFactory mongoDb() {
+        return connectionFactory().mongoDbFactory();
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return connectionFactory().redisConnectionFactory();
+    }
 }
